ce511eab3b5d0495a3529f58f4197654441cbba1
a10230m <a10230m@gmail.com>
2022-06-20 13:31:04 +0000

    もうすこし

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,54 @@
+class BooksController < ApplicationController
+
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id), notice: 'Created successfully! Thank you.'
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
+  end
+
+  def index
+    @books = Book.all
+    @user = current_user
+    @book = Book.new
+
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = current_user
+    @new = Book.new
+
+  end
+
+  def edit
+     @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: 'Updated successfully! Thank you.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :profile_image)
+  end
+end
